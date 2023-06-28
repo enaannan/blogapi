f4c9828e3f5966c35c0d9ab1ba8035c37ed22e9c
enaannan <enaannan@gmail.com>
2023-06-28 22:16:56 +0000

    finished user route

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -4,7 +4,7 @@ const router = express.Router();
 const {checkSchema} = require("express-validator");
 const {schemaFromModel} = require("../utils/modelUtils");
 
-const UserController = require("../Controllers/User");
+const UserController = require("../Controllers/userController");
 const user = require("../models/User");
 
 const userValidationSchema = schemaFromModel(user);
@@ -15,5 +15,10 @@ delete userValidationSchema.id
 delete userValidationSchema.salt
 
 router.post('/', checkSchema(userValidationSchema), UserController.createUser);
+router.get('/:id', UserController.findOne);
+router.get('/', UserController.findAll);
+router.put('/:id', checkSchema(userValidationSchema), UserController.update);
+router.delete('/:id', checkSchema(userValidationSchema), UserController.deleteById);
+router.delete('/', checkSchema(userValidationSchema), UserController.deleteAll);
 
 module.exports = router;
